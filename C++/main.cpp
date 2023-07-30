f1b6e67216443fef7d55f63590413cb26e5a4f89
River <hom0906@naver.com>
2023-07-30 16:51:36 +0900

    동적 프로그래밍 : 피보나치 수열, 모눈 종이

--- C++/main.cpp
+++ C++/main.cpp
@@ -107,3 +107,148 @@
 //	fib_history h;
 //	std::cout << Fibo(10, h) << '\n';
 //}
+
+//#include <iostream>
+//#include <map>
+//
+//using fib_history = std::map<int, int64_t>;
+//
+//int64_t Fibo(int N, fib_history& h)
+//{
+//	if (h.count(N) == 1) return h[N];
+//	if (N <= 2) return 1;
+//
+//	h[N] = Fibo(N - 1, h) + Fibo(N - 2, h);
+//	return h[N];
+//}
+//
+//int main()
+//{
+//	fib_history h;
+//	std::cout << Fibo(10, h) << '\n';
+//}
+
+//#include <iostream>
+//#include <map>
+//
+//using fib_history = std::map<int, int64_t>;
+//
+//int64_t Fibo(int N, fib_history& h)
+//{
+//	if (h.count(N) == 1) return h[N];
+//	if (N <= 2) return 1;
+//
+//	h[N] = Fibo(N - 1, h) + Fibo(N - 2, h);
+//	return h[N];
+//}
+//
+//int main()
+//{
+//	fib_history h;
+//	std::cout << Fibo(10, h) << '\n';
+//}
+
+/* < 모눈 종이 > */
+
+/*
+< 서술 >
+	#1. M x N 크기의 사각형 영역이 주어졌다.
+	#2. 오른쪽, 아래쪽으로만 이동이 가능하다.
+	#3. 좌상단에서 시작해서 우하단으로 이동하는 모든 경로는 몇개나 될까?
+
+< 입력 > : 가로( M ), 세로( N )
+< 출력 > : 경로 수( X )
+
+< 설명 > : 그림을 그려보며 나올 수 있는 경우의 수를 먼저 파악한다.
+	=> 2(M) x 2(N) = 2
+	=> 2(M) x 3(N) = 3
+	=> 2(M) x 4(N) = 4
+	=> 3(M) x 3(N) = 6
+	...
+*/
+
+//#include <iostream>
+//
+//int FindWays(int m, int n)
+//{
+//	// Base-Case
+//	if (m == 0 || n == 0) { return 0; }
+//// #. 가로, 세로에서 0 칸이라는 것은 없다. 풀수 없는 문제는 0을 반환한다.
+//	if (m == 1 || n == 1) { return 1; }
+//// #. 잘개 쪼개져 1 x 1 이 되면 단 하나의 길만 존재하게 된다.
+//// #. (&&)가 아닌(||)를 사용하는 것은 어차피 바로 위 제어문에서 0칸일 때 0을 반환하기 때문에
+////		=> 1 X 3 -> 1 X 2 -> 1 X 1이나 결국 1로 반환 되기에 굳지 밑으로 가지치기할 필요가 없다.
+//	// Recursive-Case
+//	return FindWays(m - 1, n) + FindWays(m, n - 1);
+//// #. 가로를 한 칸씩 줄여나간 경로와 세로를 한 칸씩 줄여나간 경로를 더해주면 총 경로 수가 나온다.
+//}
+//
+//int main()
+//{
+//	std::cout << FindWays(5, 5) << std::endl;
+//}
+
+//#include <iostream>
+//#include <map>			// #. 나뭇가지가 중복되는 과정이 역시 존재한다.
+//#include <string>		// #. 경우의 수를 구하는 것이지 어떤 경로로 가느냐를 구하는 것이 아니다.
+//						// #. 즉 열/행을 뒤집어도 같은 경우의 수를 구하게 된다.
+//						// #. 그러므로 f(m,n) 은 f(n,m)과 같은 경우의 수를 갖는다.
+//
+//// #. key 는 [pair 또는 string]으로 지정해 주면 된다. value 는 경로의 수를 저장하기 위해 int 로 지정해 준다.
+//using way_history = std::map<std::string, int>;
+//
+//int FindWays(int m, int n, way_history& h)
+//{	// #. map 의 key 값을 key 라는 이름의 문자열 상수로 만들어 준다.
+//	const std::string key{ std::to_string(m) + "," + std::to_string(n) };
+//
+//	// #. 이미 경로가 구해진 MxN 배열( key )은 저장된 값( value )을 돌려준다.
+//	if (h.count(key) == 1) { return h[key]; }
+//
+//	// #. NxM 배열의 key 값을 구분하기 위한 문자열 상수도 만들어 준다.
+//	const std::string rkey{ std::to_string(n) + "," + std::to_string(m) };
+//	if (h.count(rkey) == 1) { return h[rkey]; }
+//
+//	// Base-Case
+//	if (m == 0 || n == 0) { return 0; }
+//	if (m == 1 && n == 1) { return 1; }
+//
+//	// Recursive-Case
+//	h[key] = FindWays(m - 1, n, h) + FindWays(m, n - 1, h);
+//// #. 아래로 축소해 낳아가는 m - 1 과 오른쪽으로 축소해 낳아가는 n - 1 에서의 경로를 모두 합하면 최종 경로 수가 나온다.
+//// #. 경로 수를 현재 함수를 호출한 key 에 저장하고 반환한다.
+//
+//	return h[key];
+//}
+//
+//int main()
+//{
+//	way_history h;
+//	std::cout << FindWays(15, 15, h) << std::endl;
+//}
+
+//#include <iostream>
+//#include <map>
+//#include <string>
+//
+//using way_history = std::map<std::string, int>;
+//
+//int FindWays(int m, int n, way_history& h)
+//{
+//	const std::string key = std::to_string(m) + ',' + std::to_string(n);
+//	const std::string rkey = std::to_string(n) + ',' + std::to_string(m);
+//	if (h.count(key) == 1) return h[key];
+//	if (h.count(rkey) == 1)return h[rkey];
+//
+//	if (m == 0 || n == 0) return 0;
+//	if (m == 1 || n == 1) return 1;
+//
+//	h[key] = FindWays(m - 1, n, h) + FindWays(m, n - 1, h);
+//
+//	return h[key];
+//}
+//
+//int main()
+//{
+//	way_history h;
+//	std::cout << FindWays(5, 5, h) << '\n';
+//}
