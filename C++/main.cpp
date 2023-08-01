a75a4b0e1d864a4dce83c19cf711be98d1f5c5ec
River <hom0906@naver.com>
2023-08-01 18:04:20 +0900

    동적 프로그래밍 : 정수 배열

--- C++/main.cpp
+++ C++/main.cpp
@@ -148,6 +148,34 @@
 //	std::cout << Fibo(10, h) << '\n';
 //}
 
+//#include <iostream>
+//#include <map>
+//
+//using fib_history = std::map<int, int64_t>;
+//
+//int64_t Fibo(int N, fib_history& h)
+//{
+//	if (h.count(N) == 1)return h[N];
+//	if (N <= 2)return 1;
+//
+//	h[N] = Fibo(N - 1, h) + Fibo(N - 2, h);
+//	return h[N];
+//}
+
+//#include <iostream>
+//#include <map>
+//
+//using fib_history = std::map<int, int64_t>;
+//
+//int64_t Fibo(int N, fib_history& h)
+//{
+//	if (h.count(N) == 1) return h[N];
+//	if (N <= 2) return 1;
+//
+//	h[N] = Fibo(N - 1, h) + Fibo(N - 2, h);
+//	return h[N];
+//}
+
 /* < 모눈 종이 > */
 
 /*
@@ -251,4 +279,314 @@
 //{
 //	way_history h;
 //	std::cout << FindWays(5, 5, h) << '\n';
+//}
+
+//#include <iostream>
+//#include <map>
+//#include <string>
+//
+//using way_history = std::map<std::string, int>;
+//
+//int FindWays(int chng, int ho, way_history& h)
+//{
+//	const std::string key = std::to_string(chng) + ',' + std::to_string(ho);
+//	const std::string rkey = std::to_string(ho) + ',' + std::to_string(chng);
+//	if (h.count(key) == 1)return h[key];
+//	if (h.count(rkey) == 1)return h[rkey];
+//
+//	if (chng == 0 || ho == 0)return 0;
+//	if (chng == 1 || ho == 1)return 1;
+//
+//	h[key] = FindWays(chng - 1, ho, h) + FindWays(chng, ho, h);
+//
+//	return h[key];
+//}
+
+//#include <iostream>
+//#include <map>
+//#include <string>
+//
+//using way_history = std::map<std::string, int>;
+//
+//int FindWays(int Floor, int RoomNumber, way_history& h)
+//{
+//	const std::string key = std::to_string(Floor) + ',' + std::to_string(RoomNumber);
+//	const std::string rkey = std::to_string(RoomNumber) + ',' + std::to_string(Floor);
+//	if (h.count(key) == 1) return h[key];
+//	if (h.count(rkey) == 1) return h[rkey];
+//
+//	if (Floor == 0 || RoomNumber == 0) return 0;
+//	if (Floor == 1 || RoomNumber == 1) return 1;
+//
+//	h[key] = FindWays(Floor - 1, RoomNumber, h) + FindWays(Floor, RoomNumber - 1, h);
+//
+//	return h[key];
+//}
+
+/* < 정수 배열 > */
+
+/*
+< 서술 > : 주어진 동전들로 720원을 만들기 위해서는 최소 몇 개의 동전을 사용해야 하나요.
+
+< 입력 >
+	#. sum : 정수
+	#. numbers : 정수 배열
+< 출력 > : 참 or 거진
+
+< 설명 > : sum 을 만들기 위해 numbers 의 동일한 원소를 여러번 사용해도 된다.
+	=> CanAccumulate(8, { 2, 3, 5 }); : true
+	=> CanAccumulate(8, { 5, 6 });	  : false
+*/
+
+//#include <iostream>
+//#include <vector>
+//
+//// #. 결과 값에서 부터 조합할 값들을 하나씩 '빼기' 연산을 하면서 트리를 만들 수 있다.
+//// #. 만들어야 하는 결과 값을 sum 으로 지정하고, 조합할 값들을 동적 배열로 지정한다.
+//bool CanAccumulate(int sum, const std::vector<int>& numbers)
+//{
+//	// Base-Case
+//	if (sum == 0) { return true; }
+//// #. '빼기' 연산을 통해 내려가다가 값이 0 이 된다면 풀 수 있는 문제이다.
+//
+//	if (sum < 0) { return false; }
+//// #. '빼기' 연산을 통해 내려가다가 값이 0 이하가 된다면 풀 수 없는 문제이다.
+//
+//	// Recursive-Case
+//	for (auto e : numbers)
+//	{	// #. 동적 배열의 원소( 조합할 값들 )를 하나씩 '빼기' 연산한다.
+//		int remain = sum - e;
+//
+//		// #. 빼고 남은 값을 결과 값으로 보내어 재귀 호출한다.
+//		// #. 이 때 최종적으로 연산 결과 값이 0 인 나뭇가지가 생긴다면 추가적인 연산을 하지말고 반환한다.
+//		if (CanAccumulate(remain, numbers) == true)
+//		{
+//			return true;
+//		}
+//	}
+//	// #. 어떤 제어문에서도 걸리지 않은 나뭇가지는 풀 수 없는 문제이다.
+//	return false;
+//}
+//int main()
+//{
+//	std::cout << CanAccumulate(8, { 2,3,5 }) << std::endl << std::endl;
+//	std::cout << CanAccumulate(8, { 5,6 }) << std::endl << std::endl;
+//	std::cout << CanAccumulate(720, { 10,50,100,500 }) << std::endl << std::endl;
+//}
+
+//#include <iostream>
+//#include <vector>
+//#include <map>			// #. 빼기를 하다보면 동일한 나뭇가지가 나올 수 있다.
+//						// #. 이 때, 역시 계산한 값을 저장해 두었다가 중복되는 값이 나올 때 값을 반환해 준다.
+//
+//// #. Parent 노드의 값, 즉 빼고 남은 값을 key 값으로 지정하고 반환 값을 value 값으로 지정해 준다.
+//using accum_history = std::map<int, bool>;
+//
+//bool CanAccumulate(int sum, const std::vector<int>& numbers, accum_history& h)
+//{	// 이미 풀어본 문제는 저장된 값을 돌려준다.
+//	if (h.count(sum) == 1) { return h[sum]; }
+//	
+//	// Base-Case
+//	if (sum == 0) { return true; }
+//	if (sum < 0) { return false; }
+//
+//	// Recursive-Case
+//	for (auto e : numbers)
+//	{
+//		int remain = sum - e;
+//		if (CanAccumulate(remain, numbers, h) == true)
+//		{	// #. 반환값이 true 일 경우 map 에 저장해 준다.
+//			h[sum] = true;
+//			return h[sum];
+//		}
+//	}
+//	// #. 모든 원소를 한 번씩 빼면서 내려 같는데 0 의 값이 나오지 못한 경우는 false 를 저장해 준다.
+//	h[sum] = false;
+//	return h[sum];
+//}
+//int main()
+//{
+//	accum_history h;
+//
+//	std::cout << CanAccumulate(8, { 2,3,5 }, h) << std::endl << std::endl;
+//	h.clear();
+//	std::cout << CanAccumulate(900, { 7,14 }, h) << std::endl << std::endl;
+//	h.clear();
+//	std::cout << CanAccumulate(720, { 10,50,100,500 }, h) << std::endl << std::endl;
+//}
+
+//#include <iostream>
+//#include <vector>
+//#include <map>
+//
+//using accum_history = std::map<int, bool>;
+//
+//bool Accum(int sum, const std::vector<int>& numbers, accum_history& h)
+//{
+//	if (h.count(sum) == 1) return h[sum];
+//
+//	if (sum == 0) return true;
+//	if (sum < 0) return false;
+//
+//	for (auto e : numbers)
+//	{
+//		int remain = sum - e;
+//		if (Accum(remain, numbers, h))
+//		{
+//			h[sum] = true;
+//			return h[sum];
+//		}
+//	}
+//	h[sum] = false;
+//	return false;
+//}
+
+//#include <iostream>
+//#include <vector>
+//#include <map>
+//
+//using accum_history = std::map<int, bool>;
+//
+//bool Accum(int sum, const std::vector<int>& numbers, accum_history& h)
+//{
+//	if (h.count(sum) == 1) return h[sum];
+//
+//	if (sum == 0) return true;
+//	if (sum < 0) return false;
+//
+//	for (auto e : numbers)
+//	{
+//		int remain = sum - e;
+//		if (Accum(remain, numbers, h))
+//		{
+//			h[sum] = true;
+//			return h[sum];
+//		}
+//	}
+//	h[sum] = false;
+//	return h[sum];
+//}
+
+/* < 정수 배열 > */
+
+/*
+< 서술 > : 주어진 동전들로 720원을 만들기 위해서는 최소 몇 개의 동전을 사용해야 하나요.
+
+< 입력 >
+	#. sum : 정수
+	#. numbers : 정수 배열
+< 출력 > : 조합된 값의 배열
+
+< 설명 > : sum 을 만들기 위해 numbers 의 동일한 원소를 여러번 사용해도 된다.
+	=> CanAccumulate(8, { 2, 3, 5 }); : { 2, 2, 2, 2 }
+	=> CanAccumulate(8, { 5, 6 });	  : { }
+*/
+
+//#include <iostream>
+//#include <vector>
+//#include <map>
+//
+//// #. key 값은 '빼기'연산을 통해 나온 결과 값을 지정한다.
+//// #. value 값은 0 이라는 값까지 내려간 값들을 배열로 나열해야 하기 때문에 동적 배열로 지정한다.
+//using int_vector = std::vector<int>;
+//using how_accum_history = std::map<int, std::shared_ptr<int_vector>>;
+//// #. 반환 값 중 풀 수 없는 문제는 아무것도 반환해서는 않된다. 때문에 nullptr 을 반환해야 하는데
+//// #. nullptr 을 받기 위해서는 배열이 포인터 형태여야만 한다. 즉 value 는 포인터로 지정한다.
+//
+//// #. 재귀 호출로 여러 개의 함수의 반환 값을 하나의 메모리 공간에 저장하려면 shared_ptr 스마트 포인터를 사용해야 한다.
+//std::shared_ptr<int_vector> HowAccumulate(int sum, const int_vector& numbers, how_accum_history& h)
+//{	// 이미 풀은 문제는 저장된 값을 돌려준다.
+//	if (h.count(sum) == 1) { return h[sum]; }
+//
+//	// Base-Case
+//	if (sum == 0) { return std::make_shared<int_vector>(); }
+//// #. 풀 수 있는 문제는 배열 공간을 할당해 준다.
+//// #. 소유권이 shared 에 있기 때문에 make 로 할당한 메모리 공간은 if 문 가로 밖으로 나가도 유지되어 있다.
+//
+//	if (sum < 0) { return nullptr; }
+//// #. 풀 수 없는 문제는 nullptr 을 반환한다.
+//
+//	// Recursive-Case
+//	int remain{};
+//	for (auto e : numbers)
+//	{
+//		remain = sum - e;
+//		// #. shared 포인터로 반환된 값을 ret 이 받는다.
+//		auto ret = HowAccumulate(remain, numbers, h);
+//		if (ret != nullptr)
+//		{	// #. 풀 수 있는 문제의 값은 배열에 저장한다.
+//			ret->push_back(e);
+//			// #. 값을 나뭇가지 key 에 저장해 둔다.
+//			h[sum] = ret;
+//			return h[sum];
+//		}
+//	}
+//	// #. 값이 0 에 도달하지 못한 조합들은 nullptr 로 반환해 준다.
+//	h[sum] = nullptr;
+//	return h[sum];
+//}
+//void Print(const int_vector* r)
+//{
+//	std::cout << "{ ";
+//	if (r != nullptr)
+//	{
+//		for (auto e : *r) { std::cout << e << ", "; }
+//	}	std::cout << " }" << std::endl << std::endl;
+//}
+//int main()
+//{
+//	how_accum_history h;
+//
+//	Print(HowAccumulate(8, { 2,3,5 }, h).get());
+//	h.clear();
+//	Print(HowAccumulate(720, { 500,100,50,10 }, h).get());
+//	h.clear();
+//	Print(HowAccumulate(720, { 10,50,100,500 }, h).get());
+//	h.clear();
+//	Print(HowAccumulate(900, { 7,14 }, h).get());
+//}
+
+//#include <iostream>
+//#include <vector>
+//#include <map>
+//
+//using int_vec = std::vector<int>;
+//using how_accum = std::map<int, std::shared_ptr<int_vec>>;
+//
+//std::shared_ptr<int_vec> HowAccum(int sum, const int_vec& numbers, how_accum& h)
+//{
+//	if (h.count(sum) == 1) return h[sum];
+//
+//	if (sum == 0) return std::make_shared<int_vec>();
+//	if (sum < 0) return nullptr;
+//
+//	int remain{};
+//	for (auto e : numbers)
+//	{
+//		remain = sum - e;
+//		auto ret = HowAccum(remain, numbers, h);
+//		if (ret != nullptr)
+//		{
+//			ret->push_back(e);
+//			h[sum] = ret;
+//			return h[sum];
+//		}
+//	}
+//	h[sum] = nullptr;
+//	return h[sum];
+//}
+//
+//void Print(const int_vec* r)
+//{
+//	for (auto e : *r)
+//	{
+//		std::cout << e << '\n';
+//	}
+//}
+//
+//int main()
+//{
+//	how_accum h;
+//	Print(HowAccum(30, { 3,5,10 }, h).get());
+//	h.clear();
 //}
